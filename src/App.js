d95178f7e70edb0733dc2e8752098088266e4392
Hisham Wolley <hishamwolley@hotmail.com>
2021-04-23 02:13:11 +0300

    frag remove

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import Carousel from "./components/Carousel";
 
 const App = () => {
 	return (
-		<>
-			<Carousel>
-				<div style={{ background: "yellow", height: "100%" }}>
-					ScandieWeb is
-				</div>
-				<div style={{ color: "red" }}>Awesome</div>
-			</Carousel>
-		</>
+		<Carousel>
+			<div style={{ background: "yellow", height: "100%" }}>ScandieWeb is</div>
+			<div style={{ color: "red" }}>Awesome</div>
+		</Carousel>
 	);
 };
 
