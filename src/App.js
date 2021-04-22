be554bfaa718b0a81c2e19f5219b39c3772c3fcf
Hisham Wolley <hishamwolley@hotmail.com>
2021-04-23 02:28:04 +0300

    basic carousel children styles

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Carousel from "./components/Carousel";
 const App = () => {
 	return (
 		<Carousel>
-			<div style={{ background: "yellow", height: "100%" }}>ScandieWeb is</div>
-			<div style={{ color: "red" }}>Awesome</div>
+			<div style={{ background: "yellow", height: "100%" }}>
+				<h1>ScandieWeb is</h1>
+			</div>
+			<div style={{ color: "red", height: "50%", background: "gray" }}>
+				<h2>Awesome</h2>
+			</div>
 		</Carousel>
 	);
 };
